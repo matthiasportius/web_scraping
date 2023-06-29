1aed6752dec4a995fe21679cd8e7417c33112193
Matthias Portius <Matthias.Portius@gmx.de>
2023-06-29 10:51:07 +0200

    finished README cleaned up code

--- README.md
+++ README.md
@@ -1,40 +1,64 @@
-**requests** package to open website and get back its data
-**beautifulsoup** package to sort collected data from website and get desited data
+# Web Scraper
 
-tipps for scraping:
-    make it so that you only scrape what you could get as normal user (with sleep / wait functions to not bombard the server)
-    don't overgo captcha or other barriers which are there for a reason
-    don't just copy databases of a website (unspecific scraping of everything is not seen well)
-    don't share databases (especially not for money)
+Here I tried out a little bit of web scraping using **requests** and **beautifulsoup**. I was just very interested in the topic and wanted to get a hang of the basics of these two packages. That is why I also did not make requests to an API, as I wanted to scrape right of the site to learn the basics. Of course, the code needs some (a lot) of adjustment, I listed some of them in the "TODO / Ideas" heading.
+I also looked into databases and selenium while doing reasearch on web scraping. I put some notes to that in here as well.
 
-other ways of scraping data are requests to an API which gives us the asked data 
+## What are requests and beautifulsoup in simple terms?
 
-websites to scrape:
-    Indeed
-    Tripadvisor
-    Google
-    Yellowpages
-    Amazon
-    Yahoo Financ
-    Wikipedia
-    Youtube
+**requests**: package to open website and get back its data  
+**beautifulsoup**: package to sort collected data from website and get desired data
 
-Most of youtube content is created via JS - not available by BeautifulSoup; can be accessed using re.findall and json.load(matches)
+## Any basic tips on web scraping you have?
 
-program that searches for chemical:
-    can find out CAS if I give it a name
-    searches for chemical on most used suppliers (VWR, Thermo Fischer, Merck, abcr, Carlroth, ...) and compares prices
-    searches for NMR, MS spectra (or other useful info like melting point, molar mass, density, ...)
+Make it so that you only scrape what you could get as a normal user.  
+Implement sleep / wait functions to not bombard the server.  
+Don't overgo captcha or other barriers which are there for a reason.  
+Don't just copy databases of a website (unspecific scraping of everything is not seen well)  
+Don't share databases you scraped (especially not for money!)  
+Ask for permission if you feel uncertain.
 
-print request url (for example of smalles price): requests.get('url').url
+## Some notes on databases and how they are commonly used
 
-databases to store data:
-    MySQL:  good for websites, not so much features as Postgre SQL but faster, best for distributed setups and scaling (easy to upscale)
-    PostgreSQL: good for complex queries/operations (supports multiple concurrent writers), highly customizable, best for data analysis applications (read + write is fast)
-    SQLite: good for small amount of data, lightweight
-    --> use SQLite for a start, then switch to Postgres
+### MySQL  
 
-Selenium vs requests:
+- good for websites
+- not so much features as Postgre SQL but faster
+- best for distributed setups and scaling (easy to upscale)
+
+### PostgreSQL
+
+- good for complex queries/operations (supports multiple concurrent writers)
+- highly customizable, best for data analysis applications (read + write is fast)
+
+### SQLite
+
+- good for small amount of data, lightweight
+>Note: For small projects it is good to start with SQLite and then switch to Postgre if the database grows too large.
+
+## Selenium vs Requests
+
 As Selenium is a automated browser and Python Requests is a simple HTTP client, Selenium is the better option if you are scraping dynamic pages that require the page to be client side rendered before showing all the data. This is typical for websites that use modern web frameworks like AngularJS, ReactJS, VueJS, etc.
-requests for: scraping at large scale/fast, scraping API endpoints
-selenium for: rendering dynamic pages, when lot of interaction (click, scroll,...) with website is necessary, automated bots that work behind logins, screenshots of pages, heavily protected websites
+
+requests for:
+- scraping at large scale/fast
+- scraping API endpoints
+
+selenium for: 
+- rendering dynamic pages, when lot of interaction (click, scroll,...) with website is necessary
+- automated bots that work behind logins
+- screenshots of pages
+- heavily protected websites
+
+## TODO / Ideas
+
+- Replace hardcoded search-values with *argparse* functionality or some kind of GUI 
+- Check if wikipedia-url exists (name is entered correctly)
+- Add cookie to `get_prices()` abcr `requests.get()` call, since it does not work if the site has not been visited before.
+- Change output to text to output to .csv (Excel files are just better)
+- Add scraping option for Merck (search for "G" and for "€" in text)
+- Add scraping option for VWR, Thero Fischer, Carlroth, ...
+- compare prices of suppliers and return cheapest (maybe dict with gram:(price, url) and replace url if price for same g is better)
+- scrape melting point, molar mass and density from wikipedia
+- look for sites that have NMR and/or MS data that can be scraped
+- maybe use `sqlite3.Row`
+- maybe `for row in cur.execute(...):` faster as `fetchall()`
--- chem_scraper.py
+++ chem_scraper.py
@@ -3,76 +3,73 @@ import sqlite3
 import time
 from bs4 import BeautifulSoup
 
-db = sqlite3.connect('chem.db')  # connect to database (+creates if non existant)
-cur = db.cursor()  # creates cursor to execute SQL commands
-
-cur.execute("CREATE TABLE IF NOT EXISTS chemicals(name TEXT, cas TEXT PRIMARY KEY, molarmass REAL, nmr_url TEXT, ms_url TEXT)")
-cur.execute("CREATE TABLE IF NOT EXISTS prices(cas_nr TEXT, gram REAL, price REAL, besprice_url TEXT, FOREIGN KEY(cas_nr) REFERENCES chem(cas))")
-
-CHEMICALS = ['Natriumchlorid', 'Neutralrot']
-
-for chem in CHEMICALS:
-    url = f'https://de.wikipedia.org/wiki/{chem}'
-
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    wiki_data = soup.find('tbody')
-    table_data = wiki_data.text.split()
-    name = table_data[table_data.index('Name') + 1]
-    cas = table_data[table_data.index('CAS-Nummer') + 1]
-
-    cur.execute("INSERT OR IGNORE INTO chemicals (name, cas) VALUES(?, ?)", (name, cas))  # if conflict (because of unique cas value) then execute is ignored
-    db.commit()
-
-    time.sleep(3)
 
 
-cas = cur.execute("SELECT cas FROM chemicals").fetchall()
-for c in cas:
-    url = f'https://abcr.com/de_de/catalogsearch/advanced/result/?cas={c[0]}'
+def get_cas(chemicals: list[str]) -> None:
+    for chem in chemicals:
+        url = f'https://de.wikipedia.org/wiki/{chem}'
 
-    response = requests.get(url)
+        response = requests.get(url)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, 'html.parser')
+        wiki_data = soup.find('tbody')
+        table_data = wiki_data.text.split()
+        name = table_data[table_data.index('Name') + 1]
+        cas = table_data[table_data.index('CAS-Nummer') + 1]
 
-    abcr_data_price_r = soup.find_all('span', class_='price')  # class_: "sth" same as {"class": "sth"}
-    abcr_data_unit_r = soup.find_all('td', class_="align-right no-wrap")
+        cur.execute("INSERT OR IGNORE INTO chemicals (name, cas) VALUES(?, ?)", (name, cas))  # if conflict (because of unique cas value) then execute is ignored
+        db.commit()
 
-    abcr_data_price = [price.text[1:] for price in abcr_data_price_r]
-    abcr_data_unit = [gram.text.strip() for gram in abcr_data_unit_r]
-
-
-    for p, g in zip(abcr_data_price, abcr_data_unit):
-        cur.execute("INSERT OR IGNORE INTO prices VALUES(?, ?, ?, ?)", (c[0], g, p, url))
-
-    db.commit()
-
-    time.sleep(3)
+        time.sleep(3)
 
 
-with open('results.txt', 'w') as f:
-    chemical = cur.execute("SELECT name, cas FROM chemicals").fetchall()
-    for x in chemical:
-        name, cas = x
-        f.write(f"Name: {name}   CAS: {cas}\n")
-        vendor = cur.execute("SELECT gram, price, besprice_url FROM prices WHERE cas_nr = ?", (cas,)).fetchall()
+def get_prices() -> None:
+    cas = cur.execute("SELECT cas FROM chemicals").fetchall()
+    for c in cas:
+        url = f'https://abcr.com/de_de/catalogsearch/advanced/result/?cas={c[0]}'
 
-        for y in vendor:
-            unit, price, url = y
-            if unit is not None:
-                f.write(f"    {unit}  :  {price} €   {url}\n")
+        response = requests.get(url)
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+
+        abcr_data_price_r = soup.find_all('span', class_='price')  # class_: "sth" same as {"class": "sth"}
+        abcr_data_unit_r = soup.find_all('td', class_="align-right no-wrap")
+
+        abcr_data_price = [price.text[1:] for price in abcr_data_price_r]
+        abcr_data_unit = [gram.text.strip() for gram in abcr_data_unit_r]
+
+        for p, g in zip(abcr_data_price, abcr_data_unit):
+            cur.execute("INSERT OR IGNORE INTO prices VALUES(?, ?, ?, ?)", (c[0], g, p, url))
+
+        db.commit()
+
+        time.sleep(3)
+
+
+def save_to_text() -> None:
+    with open('results.txt', 'w') as f:
+        chemical = cur.execute("SELECT name, cas FROM chemicals").fetchall()
+        for x in chemical:
+            name, cas = x
+            f.write(f"Name: {name}   CAS: {cas}\n")
+            vendor = cur.execute("SELECT gram, price, besprice_url FROM prices WHERE cas_nr = ?", (cas,)).fetchall()
+
+            for y in vendor:
+                unit, price, url = y
+                if unit is not None:
+                    f.write(f"    {unit}  :  {price} €   {url}\n")
 
 
 
-# NEXT: implement (molar mass), nmr_url, ms_url, bestprice_url
+if __name__ == "__main__":
+    db = sqlite3.connect('chem.db')  # connect to database (creates if non existant)
+    cur = db.cursor()  # create cursor to execute SQL commands
 
+    cur.execute("CREATE TABLE IF NOT EXISTS chemicals(name TEXT, cas TEXT PRIMARY KEY, molarmass REAL, nmr_url TEXT, ms_url TEXT)")
+    cur.execute("CREATE TABLE IF NOT EXISTS prices(cas_nr TEXT, gram REAL, price REAL, besprice_url TEXT, FOREIGN KEY(cas_nr) REFERENCES chem(cas))")
 
-# con.row_factory = sqlite3.Row   maybe nice, cause it's a superior row form
-# select sth:
-# for row in cur.execute(...): print(row) --> different than .fetchall
+    CHEMICALS = ['Natriumchlorid', 'Neutralrot']  # preliminarily hardcoded
 
-# search on some site like: if compound found - get ...
-
-# merck: search for G in text and for € in text
-# make dict out of price and gram and replace url if price for same g is better (store every price-gram combination and make url behind that)
+    get_cas(CHEMICALS)
+    get_prices()
+    save_to_text()
